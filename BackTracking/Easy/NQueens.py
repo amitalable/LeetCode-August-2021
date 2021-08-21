3c16e82f624bb498adadf1d4ca4f683f2a1e4428
Amit Ranjan <amitalable@gmail.com>
2021-08-22 02:57:07 +0530

    NQueeens

--- BackTracking/Easy/NQueens.py
+++ BackTracking/Easy/NQueens.py
@@ -1,46 +1,43 @@
-class Solution:
-    def __init__(self) -> None:
-        self.final_list = []
+class NQueen:
+    def __init__(self, N):
+        self.board = [[0 for i in range(N)] for j in range(N)]
+        self.n = N
+        self.res = []
 
-    def NQueens(self, arr, r, n):
-        if r == n:
-            x = [[arr[i][j] for i in range(n)] for j in range(n)]
-            self.final_list.append(x)
+    def solve(self, r):
+        if r == self.n:
+            self.res.append([[self.board[i][j]
+                              for i in range(self.n)] for j in range(self.n)])
             return
+        for i in range(self.n):
+            if self.isValid(r, i):
+                self.board[r][i] = 1
+                self.solve(r+1)
+                self.board[r][i] = 0
 
-        for i in range(n):
-            if self.isSafe(arr, r, i, n):
-                arr[r][i] = 'Q'
-                self.NQueens(arr, r+1, n)
-                arr[r][i] = '-'
+    def isValid(self, row, col):
+        if self.board[row][col]:
+            return False
 
-    def isSafe(self, arr, r, c, n):
-        for i in range(r):
-            # upper
-            if arr[i][c] == 'Q':
+        for i in range(row):
+            if self.board[i][col]:
                 return False
-
-        (i, j) = (r, c)
+        (i, j) = (row, col)
         while i >= 0 and j >= 0:
-            # upper left diagonal
-            if arr[i][j] == 'Q':
+            if self.board[i][j]:
                 return False
             i -= 1
             j -= 1
-
-        (i, j) = (r, c)
-        while i >= 0 and j < n:
-            # upper right diagonal
-            if arr[i][j] == 'Q':
+        (i, j) = (row, col)
+        while i >= 0 and j < self.n:
+            if self.board[i][j]:
                 return False
             i -= 1
             j += 1
         return True
 
 
-x = 4
-arr = [['-' for i in range(x)] for i in range(x)]
-obj = Solution()
-obj.NQueens(arr, 0, x)
-for i in obj.final_list:
-    print(i)
+N = 4
+obj = NQueen(N)
+obj.solve(0)
+print(obj.res)
