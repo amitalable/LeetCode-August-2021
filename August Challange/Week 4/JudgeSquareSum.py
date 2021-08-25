a35287b2b5eb400cfefce52958eb8f77532f55b3
Amit Ranjan <amitalable@gmail.com>
2021-08-25 23:44:20 +0530

    Day 25

---
+++ August Challange/Week 4/JudgeSquareSum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        l = list(map(lambda x: x*x, range(int(c**0.5+1))))
+        start = 0
+        end = len(l)-1
+        while end >= start:
+            temp = l[end] + l[start]
+            if temp > c:
+                end -= 1
+            elif temp < c:
+                start += 1
+            else:
+                return True
+        return False
+
+
+print(Solution().judgeSquareSum(5))
