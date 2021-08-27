56128575fa65a532942a570d6778938663668288
Amit Ranjan <amitalable@gmail.com>
2021-08-27 23:55:34 +0530

    Day 27

--- August Challange/Week 4/LongestUncommonSubsequence2.py
+++ August Challange/Week 4/LongestUncommonSubsequence2.py
@@ -1,12 +1,15 @@
 # https://leetcode.com/problems/longest-uncommon-subsequence-ii/
+from typing import List
+
+
 class Solution:
     def findLUSlength(self, A: List[str]) -> int:
-        
+
         def isSubsequence(s, t):
             t = iter(t)
             return all(c in t for c in s)
-        
-        A.sort(key = len, reverse=True)
+
+        A.sort(key=len, reverse=True)
         for i, s in enumerate(A):
             found = False
             for j, ss in enumerate(A):
